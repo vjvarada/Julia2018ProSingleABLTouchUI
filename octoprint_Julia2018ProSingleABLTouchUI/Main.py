2c546117afe9868c8748c651c7b89a1884246e7c
Vijay Raghav Varada <vjvarada@yahoo.co.in>
2020-01-14 17:19:28 +0530

    added definitions for lock settings

--- octoprint_Julia2018ProSingleABLTouchUI/Main.py
+++ octoprint_Julia2018ProSingleABLTouchUI/Main.py
@@ -570,6 +570,43 @@ class MainUiClass(QtGui.QMainWindow, mainGUI_pro_single_abl.Ui_MainWindow):
         self.pgLock_bt0.clicked.connect(lambda: self.Lock_kbAdd("0"))
         self.pgLock_btBackspace.clicked.connect(lambda: self.pgLock_pin.backspace())
         self.pgLock_btSubmit.clicked.connect(self.Lock_submitPIN)
+    ''' +++++++++++++++++++++++++Lock Settings+++++++++++++++++++++++++++++++++++ '''
+    def Lock_showLock(self):
+        self.pgLock_HID.setText(str(self.__packager.hc()))
+        self.pgLock_pin.setText("")
+        if not self.__timelapse_enabled:
+            # dialog.WarningOk(self, "Machine locked!", overlay=True)
+            self.stackedWidget.setCurrentWidget(self.pgLock)
+        else:
+            # if self.__timelapse_started:
+            #     dialog.WarningOk(self, "Demo mode!", overlay=True)
+            self.stackedWidget.setCurrentWidget(self.homePage)
+
+    def Lock_kbAdd(self, txt):
+        if len(str(self.pgLock_pin.text())) < 9:
+            self.pgLock_pin.setText(str(self.pgLock_pin.text()) + txt)
+        self.pgLock_pin.setFocus()
+
+    def Lock_onPinInputChanged(self):
+        self.pgLock_btBackspace.setEnabled(len(str(self.pgLock_pin.text())) > 0)
+        self.pgLock_btSubmit.setEnabled(len(str(self.pgLock_pin.text())) > 3)
+
+    def Lock_submitPIN(self):
+        k = -1
+        t = self.pgLock_pin.text()
+        try:
+            k = int(t)
+            if self.__packager.match(k):
+                self.__packager.save(k)
+                # self.__timelapse_enabled = True
+                if dialog.SuccessOk(self, "Machine unlocked!", overlay=True):
+                    self.tellAndReboot()
+                self.stackedWidget.setCurrentWidget(self.homePage)
+            else:
+                dialog.WarningOk(self, "Incorrect unlock code")
+        except Exception as e:
+            dialog.WarningOk(self, "Error while parsing unlock code")
+            print(e.message)
 
     ''' +++++++++++++++++++++++++Print Restore+++++++++++++++++++++++++++++++++++ '''
 
