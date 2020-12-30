29d9fcd59142776289c86c07951dff9bcbddf594
ganeshmev <35722311+ganeshmev@users.noreply.github.com>
2020-12-30 17:33:58 +0530

    Print restore update
    1. print "error on Server Connected"
    2. __plugin_version__ = "1.0.1"

--- octoprint_Julia2018ProSingleABLTouchUI/Main.py
+++ octoprint_Julia2018ProSingleABLTouchUI/Main.py
@@ -636,6 +636,7 @@ class MainUiClass(QtGui.QMainWindow, mainGUI_pro_single_abl.Ui_MainWindow):
             else:
                 self.firmwareUpdateCheck()
         except:
+            print "error on Server Connected"
             pass
     ''' +++++++++++++++++++++++++Filament Sensor++++++++++++++++++++++++++++++++++++++ '''
 
--- octoprint_Julia2018ProSingleABLTouchUI/__init__.py
+++ octoprint_Julia2018ProSingleABLTouchUI/__init__.py
@@ -74,7 +74,7 @@ class Julia2018ProSingleABLTouchUI(octoprint.plugin.StartupPlugin):
 
 
 __plugin_name__ = "Julia2018ProSingleABLTouchUI"
-__plugin_version__ = "1.0.0"
+__plugin_version__ = "1.0.1"
 
 
 def __plugin_load__():
